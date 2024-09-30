649d2bfe0fe7aa0af32e444253d40eaeb4e685c0
ayeshaxshaikh <ayesha.shaikh0318@gmail.com>
2024-09-30 16:59:38 +0530

    Added Modified Functionality

--- DRS/src/components/Timer.jsx
+++ DRS/src/components/Timer.jsx
@@ -2,14 +2,15 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 import './Timer.css';
+import background_video from '../assets/background-video.mp4';
 
 const socket = io('https://drs-frontend-1.onrender.com');
+
 
 function Timer() {
   const [timer, setTimer] = useState(15);
 
   useEffect(() => {
-    
     socket.on('timerUpdate', (newTime) => {
       setTimer(newTime);
     });
@@ -23,6 +24,12 @@ function Timer() {
 
   return (
     <div className='container'>
+     
+      <video autoPlay muted loop id="background-video">
+        <source src={background_video} type="video/mp4" />
+      </video>
+
+ 
       <div className="timer-container">
         <div className='text'>
           <p>Review Timer</p>
